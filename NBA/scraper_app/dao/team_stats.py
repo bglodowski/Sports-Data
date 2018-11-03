a60819d169cc70fe69468b879f636ad19bc71f95
Brian <glodowski.brian@gmail.com>
2018-11-03 09:12:58 -0400

    First develop branch commit.

--- NBA/scraper_app/dao/team_stats.py
+++ NBA/scraper_app/dao/team_stats.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, String, Integer, Date, Numeric
 
 
-class TeamStats(base):
-	__tablename__= 'DAILY_TEAM_STATS'
+class TeamStats():
+	__tablename__ = 'DAILY_TEAM_STATS'
 
 	id = Column(Integer, primary_key=True)
 	team = Column(String)
@@ -40,9 +40,9 @@ class TeamStats(base):
 	fanduel = Column(Numeric)
 
 	def __init__(self, team, opponent, homeAway, date, pts, fg, fga, fgpercent,
-	             thrp, thrpa, thrppercent, ft, fta, ftpercent, orb, drb, trb, ast, stl,
-	             blk, tov, pf, tspercent, efgpercent, thrpar, ftr, astpercent,
-	             stlpercent, blkpercent, tovpercent, ortg, drtg, fanduel):
+				 thrp, thrpa, thrppercent, ft, fta, ftpercent, orb, drb, trb, ast, stl,
+				 blk, tov, pf, tspercent, efgpercent, thrpar, ftr, astpercent,
+				 stlpercent, blkpercent, tovpercent, ortg, drtg, fanduel):
 		self.team = team
 		self.opponent = opponent
 		self.homeAway = homeAway
@@ -53,5 +53,29 @@ class TeamStats(base):
 		self.fgpercent = fgpercent
 		self.thrp = thrp
 		self.thrpa = thrpa
+		self.thrppercent = thrppercent
+		self.ft = ft
+		self.fta = fta
+		self.ftpercent = ftpercent
+		self.orb = orb
+		self.drb = drb
+		self.trb = trb
+		self.ast = ast
+		self.stl = stl
+		self.blk = blk
+		self.tov = tov
+		self.pf = pf
+		self.tspercent = tspercent
+		self.efgpercent = efgpercent
+		self.thrpar = thrpar
+		self.ftr = ftr
+		self.astpercent = astpercent
+		self.stlpercent = stlpercent
+		self.blkpercent = blkpercent
+		self.tovpercent = tovpercent
+		self.ortg = ortg
+		self.drtg = drtg
+		self.fanduel = fanduel
+
 
 
