6a567c07a9e3d9562d2f9af2190bf383a4632f74
bglodowski <glodowski.brian@gmail.com>
2018-11-15 18:53:37 +0000

    11/15 commit 1

--- NBA/scraper_app/main.py
+++ NBA/scraper_app/main.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from multiprocessing import Pool
 
-from scraper import get_soup, get_game_links, get_daily_data
+from scraper import get_game_links, get_daily_data
 from helpermethods import get_last_processing_date, update_processing_date
 
 # Get last processing date(+1) and make payload for GET request, loop through days until yesterdays date is hit
@@ -11,9 +11,9 @@ while True:
 	date = get_last_processing_date() + timedelta(days_forward)
 	payload = {'month': str(date.month), 'day': str(date.day), 'year': str(date.year)}
 	links = get_game_links(payload)
-	with Pool as p:
-		p.map(links, get_daily_data())
-
+	print(links)
+# 	with Pool as p:
+# 		p.map(links, get_daily_data())
 	if date == yesterdays_date:
 		update_processing_date(date.strftime('%Y-%m-%d'))
 		break
--- NBA/scraper_app/scraper.py
+++ NBA/scraper_app/scraper.py
@@ -5,7 +5,7 @@ import requests
 def get_soup(params=None):
 	try:
 		response = requests.get('https://www.basketball-reference.com/boxscores', params)
-		soup = BeautifulSoup(response)
+		soup = BeautifulSoup(response.text, 'lxml')
 	except ConnectionError:
 		print('Unable to connect to website.')
 	except TimeoutError:
@@ -14,14 +14,15 @@ def get_soup(params=None):
 
 
 def get_daily_data(payload):
+	return None
+
+
+
+def get_game_links(payload):
 	links = []
 	soup = get_soup(payload)
 	for link in soup.find_all('a', string='Box Score'):
-		links.append(link.get('href'))
+	    tail = link.get('href')
+	    url = 'https://www.basketball-reference.com{}'.format(tail)
+	    links.append(url)
 	return links
-
-
-
-
-def get_game_links(soup):
-	return None
