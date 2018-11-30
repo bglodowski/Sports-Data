13686b46fd2a17f97be19b9a5df6af8c093d47b4
bglodowski <glodowski.brian@gmail.com>
2018-11-30 21:08:03 +0000

    dbconnections updated

--- NBA/scraper_app/dbconnection.py
+++ NBA/scraper_app/dbconnection.py
@@ -3,16 +3,23 @@ import sshtunnel
 
 USERNAME = 'bglodowski'
 PASSWORD =
-HOST =
-DB =
+HOST = 'bglodowski.mysql.pythonanywhere-services.com'
+DB = 'bglodowski$NBA_DATA'
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 def get_connection():
     try:
-        conn = pymysql.connect(host=HOST,
-        password=PASSWORD,
-        user=HOST,
-        db=db)
-
+        conn = pymysql.connect(host=HOST, user=USERNAME, password=PASSWORD, db=DB)
         return conn
     except Exception as e:
-        with
+        with sshtunnel.SSHTunnelForwarder(('ssh.pythonanywhere.com'), ssh_username=USERNAME, ssh_password=PASSWORD, remote_bind_address=(HOST, 3306)) as tunnel:
+            conn = pymysql.connect(host='127.0.0.1', port=tunnel.local_bind_port, user=USERNAME, password=PASSWORD, db=DB)
+            return conn
+
+def commit_data(cursor, stat_list):
+    query = "INSERT INTO DAILY_NBA_ (id, )"
+
+
+#TODO Add days since last game column
